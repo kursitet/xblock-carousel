b97b73080e577ab766ae526f23165e3bfc6d2f89
Eugene Medvedev <rn3aoh.g@gmail.com>
2016-04-20 19:35:19 +0300

    Explicit utf-8 decode.

--- carousel/utils.py
+++ carousel/utils.py
@@ -3,8 +3,8 @@ import pkg_resources
 from django.template import Context, Template
 
 def load_resource(resource_path):
-    resource_content = resource_content = pkg_resources.resource_string(__name__, resource_path)
-    return unicode(resource_content)
+    resource_content = pkg_resources.resource_string(__name__, resource_path)
+    return resource_content.decode('utf-8')
 
 def render_template(template_path, context={}):
     template_str = load_resource(template_path)
